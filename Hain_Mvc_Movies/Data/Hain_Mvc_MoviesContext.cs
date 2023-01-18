3a9e665b05006446678ea8e996b173b48139d662
Mihkel Hain <Mihkelhain7@gmail.com>
2023-01-18 20:38:44 +0200

    add a model

---
+++ Hain_Mvc_Movies/Data/Hain_Mvc_MoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hain_Mvc_Movies.Models;
+
+namespace Hain_Mvc_Movies.Data
+{
+    public class Hain_Mvc_MoviesContext : DbContext
+    {
+        public Hain_Mvc_MoviesContext (DbContextOptions<Hain_Mvc_MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hain_Mvc_Movies.Models.Movie> Movie { get; set; }
+    }
+}
